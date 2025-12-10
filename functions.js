4becf5216d3ba230586efbcc05ef5c5758ea1457
AnabelleA <anabelleahteck@gmail.com>
2025-12-10 17:32:59 -0500

    added project template page

---
+++ functions.js
@@ -0,0 +1,36 @@
+//functions for website
+
+const slides = document.querySelectorAll(".slides img");
+let slideIndex = 0;
+let intervalId = null;
+
+
+document.addEventListener("DOMContentLoaded", initializeSlider);
+
+function initializeSlider(){
+    slides[slideIndex].classList.add("displaySlide");
+}
+
+function showSlide(index){
+    if(index >= slides.length){
+        slideIndex=0;
+    }
+    else if(index < 0){
+        slideIndex = slides.length -1;
+    }
+
+    slides.forEach(slide => {
+        slide.classList.remove("displaySlide")
+    })
+    slides[slideIndex].classList.add("displaySlide")
+}
+
+function prevSlide(){
+    slideIndex--;
+    showSlide(slideIndex);
+}
+
+function nextSlide(){
+    slideIndex++;
+    showSlide(slideIndex);
+}
